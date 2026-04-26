49d480f6433ab5034556cfb102fdac8f1e0b43b2
EvilNick2 <evilnick002@gmail.com>
2026-04-26 21:57:00 +0100

    feat(css): add root variables and layout styling foundation

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './css/tokens.css'
+import './css/layout.css'
 import './index.css'
 import App from './App.tsx'
 
