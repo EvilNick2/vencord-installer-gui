0ad863829980d60596e1a41ba7edcd6af14cc776
EvilNick2 <evilnick002@gmail.com>
2026-05-09 23:50:59 +0100

    feat(windows): require app the be run as administrator on windows
    this ensures that no permission issues happen when running the install
    steps

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,9 @@
 fn main() {
-  tauri_build::build()
+    #[cfg(windows)]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_manifest_file("app.manifest");
+        res.compile().unwrap();
+    }
+    tauri_build::build()
 }
